9788e0998f0fc53d70432873d0b233a285cbdeaa
laavannyasharma <77400099+laavannyasharma@users.noreply.github.com>
2021-05-14 20:37:23 +0530

    Add files via upload

---
+++ Project-Template-PRO-C35-main/sketch.js
@@ -0,0 +1,72 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database
+var position
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+ var balloonPosition = database.ref("balloon/height")
+ balloonPosition.on("value",readPosition,showError)
+}
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    updateHeight(-1,0)
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    updateHeight(+1,0)
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    updateHeight(0,-1)
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    updateHeight(0,+1)
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+    x:balloon.x+x,
+    y:balloon.y+y,
+  })
+}
+
+function readPosition(data){
+position = data.val()
+balloon.y = position.y
+balloon.x = position.x
+}
+
+function showError(){
+  console.log("error")
+}
